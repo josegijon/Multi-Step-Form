12b4b137078ba7608830369e6e6b22c0e88f57b2
jose <jgijalm388@g.educaand.es>
2026-01-20 21:02:54 +0100

    feat: implement navigation logic in FormNavigation and integrate it into MultiStepForm

--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -5,14 +5,21 @@ import { Step2Professional } from "./steps/Step2Professional"
 import { Step3Preferences } from "./steps/Step3Preferences"
 import { Step4Summary } from "./steps/Step4Summary"
 import { Header } from './Header';
+import { useState } from "react"
 
 export const MultiStepForm = () => {
+
+    const [currentStep, setCurrentStep] = useState(4);
+
     return (
-        <main className="font-inter bg-[#f3f4f6] min-h-screen flex flex-col gap-4">
+        <main className="font-inter bg-[#f3f4f6] min-h-screen flex flex-col gap-4 pb-8">
             <Header />
             <ProgressBar />
-            <Step2Professional />
-            <FormNavigation />
+            {currentStep === 1 ? <Step1Personal /> : ''}
+            {currentStep === 2 ? <Step2Professional /> : ''}
+            {currentStep === 3 ? <Step3Preferences /> : ''}
+            {currentStep === 4 ? <Step4Summary /> : ''}
+            <FormNavigation currentStep={currentStep} />
         </main>
     )
 }
