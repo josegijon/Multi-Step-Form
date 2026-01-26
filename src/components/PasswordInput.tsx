2dd920a43df230da6e264d5d3cd0b7c05dda42e6
jose <jgijalm388@g.educaand.es>
2026-01-26 13:48:02 +0100

    refactor: move getInputStyles function to a new styles utility file and update FormInput and PasswordInput components

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import type { FormDataType } from "../types/form.types";
 import { getFieldError } from "../utils/validation";
 import { Eye, EyeClosed } from "lucide-react";
+import { getInputStyles } from "../utils/styles";
 
 interface PasswordInputProps {
     id: "password" | "confirmPassword";
@@ -12,14 +13,6 @@ interface PasswordInputProps {
     touched: boolean;
     formData?: FormDataType;
 }
-
-const getInputStyles = (hasError: boolean) =>
-    `w-full h-12 px-4 pr-12 rounded-md border bg-white text-black-01 text-base transition-all
-    placeholder:text-[#9ca3af] focus:outline-none focus:ring-2
-    ${hasError
-        ? "border-red-500 focus:ring-red-500/50 focus:border-red-500"
-        : "border-[#dbe0e6] focus:ring-blue-primary/50 focus:border-blue-primary"
-    }`;
 
 export const PasswordInput = ({
     id,
